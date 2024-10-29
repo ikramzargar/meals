08b2af92c94b7dcb368fd3cefcb96858a965e422
ikramzargar <ikramxargar@gmail.com>
2024-10-29 13:00:07 +0530

    complete skeleton

---
+++ lib/screens/tabs.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:meals/screens/categories.dart';
+import 'package:meals/screens/meals.dart';
+
+class TabsScreen extends StatefulWidget {
+  const TabsScreen({super.key});
+
+  @override
+  State<TabsScreen> createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  int selectedPageIndex = 0;
+  void _selectPage(int index) {
+    setState(() {
+      _selectPage(index);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget activePage = CategoriesScreen();
+    var activePageTitle = 'Categories';
+    if (selectedPageIndex == 1) {
+      activePage = const MealsScreen(
+        title: 'Favourites',
+        meals: [],
+      );
+      activePageTitle = 'Favourites';
+    }
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(activePageTitle),
+      ),
+      body: activePage,
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.set_meal),
+            label: 'Categories',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star),
+            label: 'Favourites',
+          ),
+        ],
+      ),
+    );
+  }
+}
